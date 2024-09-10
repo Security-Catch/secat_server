4c2c1d20b9c960fb1ca64b71af48595bc2bdefe1
LimJeonghyun <ljh01030913@gmail.com>
2024-09-10 18:10:22 +0900

    fix : Api Error Handling

--- index.js
+++ index.js
@@ -5,35 +5,49 @@ const port = process.env.PORT || 3000;
 
 app.get("/smishing/check/", async (req, res) => {
   try {
-    const response = await axios.get("http://127.0.0.1:8000/detect", {
+    const response = await axios.get("http://200.5.60.236:3030/smishing/check/", {
       params: {
         message: req.query.message,
       },
     });
-    res.send(response.data); // 인공지능 파트에서 받은 값 전달
-    console.log(req.query.message); // 앱파트에서 받은 메시지 체크
-    console.log(response.data); // 인공지능 서버에서 받는 값 체크
+
+    if (response.data.code == 200) {
+      res.send(response.data);
+      console.log(req.query.message); // 앱파트에서 받은 메시지 체크
+      console.log(response.data); // 인공지능 서버에서 받는 값 체크
+    }
+    else {
+      res.status(400).send({ error : "잘못된 응답 코드"});
+    }
+
+    
   } catch (error) {
-    console.error(error);
+    console.error("에러 발생:", error.message);
+    res.status(500).send({ error: "서버 오류 발생" });
   }
-  console.log(req.body);
 });
 
 // 스미싱 신고
 app.get("/smishing/report/", async (req, res) => {
   try {
-    const response = await axios.get("http://127.0.0.1:8000/detect", {
+    const response = await axios.get("http://200.5.60.236:3030/smishing/check/", {
       params: {
         message: req.query.message,
       },
     });
-    res.send(response.data); // 인공지능 파트에서 받은 값 전달
-    console.log(req.query.message); // 앱파트에서 받은 메시지 체크
-    console.log(response.data); // 인공지능 서버에서 받는 값 체크
+
+    if (response.data.code == 200) {
+      res.send(response.data);
+      console.log(req.query.message); // 앱파트에서 받은 메시지 체크
+      console.log(response.data); // 인공지능 서버에서 받는 값 체크
+    }
+    else {
+      res.status(400).send({ error : "잘못된 응답 코드 ${code}"});
+    }
   } catch (error) {
-    console.error(error);
+    console.error("에러 발생:", error.message);
+    res.status(500).send({ error: "서버 오류 발생" });
   }
-  console.log(req.body);
 });
 
 app.listen(port, () => {
